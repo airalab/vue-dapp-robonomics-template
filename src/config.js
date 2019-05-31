a8578e4a62c2d6eceefa2b44764af2468c6ccdf9
vol4tim <vol4tim@gmail.com>
2019-05-31 14:59:12 +0400

    remove result hash
    Signed-off-by: vol4tim <vol4tim@gmail.com>

--- src/config.js
+++ src/config.js
@@ -15,7 +15,6 @@ export const ROBONOMICS = {
 
 export const MODEL = 'QmfCcLKrTCuXsf6bHbVupVv4zsbs6kjqTQ7DRftGqMLjdW';
 export const OBJECTIVE = 'Qmbm3o2wkqseSEi5F69CPAuDrsKnrwTJ3HN5FVLPgLHKUm';
-export const RESULT = 'QmRnbvYL4ehmVnuXQbB54ZvSzrrWRxVsBBx1gKbDKLd6dK';
 export const PRICE = 0;
 
 export const IPFS_CONFIG = {
